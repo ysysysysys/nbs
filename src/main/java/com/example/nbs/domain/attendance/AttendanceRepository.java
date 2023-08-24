ae3c405fd4e168aacd17ce04da00944499f834a2
ysysysysys <y.u.r.8c.p2w@gmail.com>
2023-08-24 16:14:20 +0900

    sqlのcount(*)の記法を修正

--- src/main/java/com/example/nbs/domain/attendance/AttendanceRepository.java
+++ src/main/java/com/example/nbs/domain/attendance/AttendanceRepository.java
@@ -18,7 +18,7 @@ public interface AttendanceRepository {
     @Select("select * from nbs.attendance where notice_id = #{noticeId}")
     AttendanceEntity findByNoticeId(long noticeId);
 
-    @Select("select count(*) from nbs.attendance where notice_id = #{noticeId} and user_id = #{userId}")
+    @Select("select count(notice_id) from nbs.attendance where notice_id = #{noticeId} and user_id = #{userId}")
     int count(long noticeId, long userId);
 
     @Select("select u.fullname,(case a.attendance_check when 1 then '出席' else '欠席' end) as reply from nbs.user u left join nbs.attendance a on u.id = a.user_id and a.notice_id = #{noticeId}")
