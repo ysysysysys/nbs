274e0140f6064ad823648c37d9652813d54874e1
ysysysysys <y.u.r.8c.p2w@gmail.com>
2023-08-25 17:47:02 +0900

    UserRepository.xml追加 (XMLファイルを作成し、Mapperインターフェースと紐づけ)

--- src/main/java/com/example/nbs/domain/auth/UserRepository.java
+++ src/main/java/com/example/nbs/domain/auth/UserRepository.java
@@ -8,31 +8,22 @@ import java.util.Optional;
 @Mapper
 public interface UserRepository {
 
-    @Select("select * from nbs.user where username = #{username}")
-    Optional<UserEntity> findByUsername(String username);
-
-    @Select("select * from nbs.user")
     List<UserEntity> findAll();
 
-    @Insert("insert into nbs.user (username,password,authority,fullname,address,created_datetime,updated_datetime) value (#{username}, #{password},#{authority},#{fullname},#{address},#{dt},#{dt})")
-    void insert(String username, String password, String authority, String fullname, String address, String dt);
-
-    @Select("select * from nbs.user where id = #{id}")
     UserEntity findByUserId(long id);
 
-    @Update("update nbs.user set authority = #{authority}, updated_datetime = #{dt} where id = #{id}")
+    Optional<UserEntity> findByUsername(String username);
+
+    void insert(String username, String password, String authority, String fullname, String address, String dt);
+
     void updateAuthority(long id, String authority, String dt);
 
-    @Update("update nbs.user set fullname = #{fullname}, address = #{address}, updated_datetime = #{dt} where id = #{id}")
     void updateBasicInfo(long id, String fullname, String address, String dt);
 
-    @Update("update nbs.user set username = #{username}, updated_datetime = #{dt} where id = #{id}")
     void updateUsername(long id, String username, String dt);
 
-    @Update("update nbs.user set password = #{password}, updated_datetime = #{dt} where id = #{id}")
     void updatePassword(long id, String password, String dt);
 
-    @Delete("delete from nbs.user where id = #{id}")
     void delete(long id);
 
 }
