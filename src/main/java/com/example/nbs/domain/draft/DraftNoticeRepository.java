e2e2e007ca639d04a1b1e75db190d1716390bad1
ysysysysys <y.u.r.8c.p2w@gmail.com>
2023-08-25 14:34:21 +0900

    DraftNoticeRepository.xml追加 (XMLファイルを作成し、Mapperインターフェースと紐づけ)

--- src/main/java/com/example/nbs/domain/draft/DraftNoticeRepository.java
+++ src/main/java/com/example/nbs/domain/draft/DraftNoticeRepository.java
@@ -8,19 +8,14 @@ import java.util.List;
 @Mapper
 public interface DraftNoticeRepository {
 
-    @Select("select * from nbs.draft_notice")
     List<NoticeEntity> findAll();
 
-    @Insert("insert into nbs.draft_notice (id,title,contents,request_for_reply,created_user_id,updated_user_id,created_datetime,updated_datetime) value (#{id},#{title},#{contents},#{request_for_reply},#{loginId},#{loginId},#{dt},#{dt})")
-    void insert(long id, String title, String contents, int request_for_reply, long loginId, String dt);
-
-    @Select("select * from nbs.draft_notice where id = #{noticeId}")
     NoticeEntity findByDraftId(long noticeId);
 
-    @Update("update nbs.draft_notice set title = #{title},contents = #{contents},request_for_reply = #{request_for_reply},updated_user_id = #{loginId},updated_datetime = #{dt} where id = #{id}")
+    void insert(long id, String title, String contents, int request_for_reply, long loginId, String dt);
+
     void update(long id, String title, String contents, int request_for_reply, long loginId, String dt);
 
-    @Delete("delete from nbs.draft_notice where id = #{id}")
     void delete(long id);
 
 }
