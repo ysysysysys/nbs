b1bd7671ae7bfd591b96c742652fbcdf4fa99666
ysysysysys <y.u.r.8c.p2w@gmail.com>
2023-08-25 15:05:31 +0900

    FileRepository.xml,DraftFileRepository.xml追加 (XMLファイルを作成し、Mapperインターフェースと紐づけ)

--- src/main/java/com/example/nbs/domain/draft/DraftFileRepository.java
+++ src/main/java/com/example/nbs/domain/draft/DraftFileRepository.java
@@ -5,10 +5,8 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface DraftFileRepository {
 
-    @Insert("insert into nbs.draft_file (notice_id,name,created_datetime,updated_datetime) value (#{noticeLastId},#{filename},#{dt},#{dt})")
     void insert(long noticeLastId, String filename, String dt);
 
-    @Delete("delete from nbs.draft_file where notice_id = #{id}")
     void delete(long id);
 
 }
--- src/main/java/com/example/nbs/domain/notice/FileRepository.java
+++ src/main/java/com/example/nbs/domain/notice/FileRepository.java
@@ -2,15 +2,11 @@ package com.example.nbs.domain.notice;
 
 import org.apache.ibatis.annotations.*;
 
-import java.util.List;
-
 @Mapper
 public interface FileRepository {
 
-    @Insert("insert into nbs.file (notice_id,name,created_datetime,updated_datetime) value (#{noticeLastId},#{filename},#{dt},#{dt})")
     void insert(long noticeLastId, String filename, String dt);
 
-    @Delete("delete from nbs.file where notice_id = #{id}")
     void delete(long id);
 
 }
