0d2108c31c9bc87b58d6019530c3a077752f9a05
ysysysysys <y.u.r.8c.p2w@gmail.com>
2023-08-30 23:09:16 +0900

    gradleコマンドからjarファイルを作成する際に変更した点

--- src/main/java/com/example/nbs/NbsApplication.java
+++ src/main/java/com/example/nbs/NbsApplication.java
@@ -2,26 +2,28 @@ package com.example.nbs;
 
 import com.example.nbs.web.uploadingfiles.storage.StorageProperties;
 import com.example.nbs.web.uploadingfiles.storage.StorageService;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.nbs")
 @EnableConfigurationProperties(StorageProperties.class)
+@MapperScan("com.example.nbs.domain")
 public class NbsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NbsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NbsApplication.class, args);
+    }
 
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-			storageService.deleteAll();
-			storageService.init();
-		};
-	}
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 
 }
